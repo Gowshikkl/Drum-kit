1859efe30c2b969a84b871a9b4f52a11f469579a
Gowshikkl <108187444+Gowshikkl@users.noreply.github.com>
2022-07-14 14:04:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,99 @@
+document.addEventListener("keypress",function(event){
+    var key1 = event.key;
+    buttonAnimation(key1);
+    switch(key1 ){
+        case "w" :
+            var tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+    
+        case "a" :
+            var tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+        
+        case "s" :
+            var tom3= new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+        
+        case "d" :
+            var tom4= new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+        
+        case "j" :
+                var snare= new Audio('sounds/snare.mp3');
+                snare.play();
+                break;
+    
+         case "k" :
+                var kickBass= new Audio('sounds/kick-bass.mp3');
+                kickBass.play();
+                break;
+    
+         case "l" :
+                 var crash = new Audio('sounds/crash.mp3');
+                 crash.play();
+                 break;
+    
+         default:        
+                
+    }
+})
+for(var i = 0;i<=6;i++)
+{
+document.querySelectorAll("button")[i].addEventListener("click",function(){
+var buttonName = this.innerHTML;
+buttonAnimation(buttonName);
+switch(buttonName ){
+    case "w" :
+        var tom1 = new Audio('sounds/tom-1.mp3');
+        tom1.play();
+        break;
+
+    case "a" :
+        var tom2 = new Audio('sounds/tom-2.mp3');
+        tom2.play();
+        break;
+    
+    case "s" :
+        var tom3= new Audio('sounds/tom-3.mp3');
+        tom3.play();
+        break;
+    
+    case "d" :
+        var tom4= new Audio('sounds/tom-4.mp3');
+        tom4.play();
+        break;
+    
+    case "j" :
+            var snare= new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+
+     case "k" :
+            var kickBass= new Audio('sounds/kick-bass.mp3');
+            kickBass.play();
+            break;
+
+     case "l" :
+             var crash = new Audio('sounds/crash.mp3');
+             crash.play();
+             break;
+
+     default:        
+            
+}
+
+});
+}
+function buttonAnimation(keypressed){
+    var keyForAnimation = document.querySelector("."+ keypressed);
+    keyForAnimation.classList.add("pressed");
+
+    setTimeout(function(){
+        keyForAnimation.classList.remove("pressed");
+    },100);
+
+}
